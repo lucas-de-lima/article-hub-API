837396ae30f71e02749fe25bd85307ed5665a0b7
Lucas-De-Lima <lucasramosdelima@gmail.com>
2023-03-12 13:21:06 -0300

    req 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define(
+    "PostCategory",
+    {
+      postId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      categoryId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      
+    },
+    {
+      underscored: true,
+      timestamps: false,
+    }
+  );
+  PostCategory.associate = ({Category, BlogPost}) => {
+    Category.belongsToMany(BlogPost, {
+        foreignKey: "category_id",
+        otherKey: "post_id",
+        through: "PostCategory",
+        as: "posts"
+    });
+    BlogPost.belongsToMany(Category, {
+        foreignKey: "post_id",
+        otherKey: "category_id",
+        through: "PostCategory",
+        as: "categories"
+    });
+  };
+  return PostCategory;
+};
